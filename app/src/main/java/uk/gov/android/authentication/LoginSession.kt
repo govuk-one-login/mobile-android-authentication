51de576a6b844df902e02e13f3d8c30ee2f78259
abradbury <18612852+alex-bradbury@users.noreply.github.com>
2023-12-21 09:28:22 +0000

    feat: Refactor Login Session
    - Add enums for some configuration fields
    - Add custom error, `AuthenticationError`
    - Remove init function as can pass context into constructor
    Resolves: DCMAW-7374

--- app/src/main/java/uk/gov/android/authentication/LoginSession.kt
+++ app/src/main/java/uk/gov/android/authentication/LoginSession.kt
@@ -1,8 +1,6 @@
 package uk.gov.android.authentication
 
-import android.content.Context
 import android.content.Intent
-import java.lang.Exception
 
 /**
  * This class provides a wrapper for [net.openid.appauth](https://github.com/openid/AppAuth-Android)
@@ -20,18 +18,11 @@ interface LoginSession {
     )
 
     /**
-     * Call after creation to create the [net.openid.appauth.AuthorizationService] instance
-     *
-     * @param context Used to create the AuthorizationService
-     */
-    fun init(context: Context): LoginSession
-
-    /**
      * Callback function to handle intent from the activity result started by [present]
      *
      * @param intent The intent from the login activity result
      * @param callback Method to extract and handle local token usage/storage
-     * @throws [Exception] if Authorization fails
+     * @throws [AuthenticationError] if Authorization fails
      */
     @Throws(Exception::class)
     fun finalise(intent: Intent, callback: (tokens: TokenResponse) -> Unit)
