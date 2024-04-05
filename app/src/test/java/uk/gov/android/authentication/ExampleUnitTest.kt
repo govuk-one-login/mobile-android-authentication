da576738902ae90ac078371117b15467641c8030
Geoffrey Sato-Holt <geoffrey.satoholt@digital.cabinet-office.gov.uk>
2024-04-05 15:35:31 +0100

    adding another androidTests path

--- app/src/test/java/uk/gov/android/authentication/ExampleUnitTest.kt
+++ app/src/test/java/uk/gov/android/authentication/ExampleUnitTest.kt
@@ -1,15 +1,8 @@
 package uk.gov.android.authentication
 
-<<<<<<< HEAD
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-=======
-import org.junit.Test
-
-import org.junit.Assert.*
-
->>>>>>> 8ef62cd (Initial Commit)
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -20,8 +13,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 8ef62cd (Initial Commit)
