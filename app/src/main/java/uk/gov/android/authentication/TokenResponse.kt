3b39190468d2ac1f1ace48a8c8f8a53a588eea94
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-08-20 16:09:27 +0100

    test: Update TokenResponse for increased code coverage
    Move TokenResponse testing helper functions into TokenResponseTest
    Update testing helper function to not catch exceptions
    Fix tests to check exceptions are thrown
    Add tests to cover default parameter serialisation and de-serialisation of TokenResponse
    Swap to using kotlin test library where possible
    
    Technically this decreases total code coverage percentage, because TokenResponse gets smaller, but helps in the long run
    
    Signed-off-by: ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>

--- app/src/main/java/uk/gov/android/authentication/TokenResponse.kt
+++ app/src/main/java/uk/gov/android/authentication/TokenResponse.kt
@@ -1,10 +1,6 @@
 package uk.gov.android.authentication
 
-import android.util.Log
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.SerializationException
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 @Serializable
 data class TokenResponse(
@@ -13,22 +9,4 @@ data class TokenResponse(
     val accessTokenExpirationTime: Long,
     val idToken: String? = null,
     val refreshToken: String? = null
-) {
-    fun jsonSerializeString(): String {
-        return Json.encodeToString(this)
-    }
-
-    companion object {
-        fun jsonDeserialize(text: String): TokenResponse? {
-            return try {
-                Json.decodeFromString(text)
-            } catch (e: SerializationException) {
-                Log.e(this::class.java.simpleName, e.message, e)
-                null
-            } catch (e: IllegalArgumentException) {
-                Log.e(this::class.java.simpleName, e.message, e)
-                null
-            }
-        }
-    }
-}
+)
