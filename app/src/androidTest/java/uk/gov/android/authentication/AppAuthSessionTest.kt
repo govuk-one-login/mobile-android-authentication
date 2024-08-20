c098d3d9fb92a197d89c1615833b5153445cf7cb
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-08-20 21:39:05 +0100

    test: Update AppAuthSession to use previous changes
    Update AppAuthSession to use functions added and tested in previous commits
    Update AppAuthSessionTest to match previous tests done
    
    Ironically the code coverage goes slightly down with this (code removal), but the branch coverage increases

--- app/src/androidTest/java/uk/gov/android/authentication/AppAuthSessionTest.kt
+++ app/src/androidTest/java/uk/gov/android/authentication/AppAuthSessionTest.kt
@@ -1,99 +1,132 @@
 package uk.gov.android.authentication
 
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import androidx.activity.result.ActivityResultLauncher
+import androidx.test.platform.app.InstrumentationRegistry
+import net.openid.appauth.AuthorizationException
+import net.openid.appauth.AuthorizationResponse
 import net.openid.appauth.AuthorizationService
 import net.openid.appauth.AuthorizationService.TokenResponseCallback
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThrows
+import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import kotlin.test.assertEquals
 
+/* What can't be checked here:
+* Full route of AppAuthSession::present
+* Full route of AppAuthSession::finalise
+* Because the real AuthorizationService has to have a browser available from the context
+*  */
 class AppAuthSessionTest {
-    private val mockContext: Context = mock()
-    private val mockAuthService: AuthorizationService = mock()
-    private val mockLauncher: ActivityResultLauncher<Intent> = mock()
+    private lateinit var appAuthSession: AppAuthSession
+    private lateinit var authService: AuthorizationService
 
-    private val loginSession: LoginSession = AppAuthSession(
-        mockContext,
-        mockAuthService
-    )
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        authService = mock()
+        appAuthSession = AppAuthSession(context, authService)
+    }
 
     @Test
-    fun presentLaunchesAuthRequest() {
-        val authUri = Uri.parse("https://example.com/authorize")
-        val redirectUri = Uri.parse("https://example.com/redirect")
-        val tokenUri = Uri.parse("https://example.com/token")
-        val loginSessionConfiguration = LoginSessionConfiguration(
-            authorizeEndpoint = authUri,
-            clientId = "sampleClientId",
-            redirectUri = redirectUri,
-            scopes = listOf(LoginSessionConfiguration.Scope.OPENID),
-            tokenEndpoint = tokenUri,
-            persistentSessionId = "persistentId"
+    fun presentCreatesAndLaunchesAuthorisationIntent() {
+        // GIVEN a mock AuthorizationService, a mock ActivityResultLauncher, and a real LoginSessionConfiguration
+        whenever(authService.getAuthorizationRequestIntent(any()))
+            .thenReturn(Intent(Intent.ACTION_VIEW))
+
+        val launcher: ActivityResultLauncher<Intent> = mock()
+        whenever(launcher.launch(any())).then {  } // take no action
+
+        val loginSessionConfig = LoginSessionConfiguration(
+            authorizeEndpoint = Uri.parse("https://auth.gov.uk/test"),
+            clientId = "clientId.Test",
+            locale = LoginSessionConfiguration.Locale.EN,
+            prefersEphemeralWebSession = true,
+            redirectUri = Uri.parse("https://redirect.gov.uk/test"),
+            responseType = LoginSessionConfiguration.ResponseType.CODE,
+            scopes = emptyList(),
+            tokenEndpoint = Uri.parse("https://token.gov.uk/test"),
+            vectorsOfTrust = "[\"Cl.Cm.P0\"]",
+            persistentSessionId = "persistentSessionTestId"
         )
-
-        val expectedIntent = Intent(Intent.ACTION_VIEW)
-        whenever(mockAuthService.getAuthorizationRequestIntent(any()))
-            .thenReturn(expectedIntent)
-
-        loginSession.present(mockLauncher, loginSessionConfiguration)
-
-        verify(mockLauncher).launch(expectedIntent)
+        // WHEN present is called
+        appAuthSession.present(
+            launcher = launcher,
+            configuration = loginSessionConfig
+        )
+        // THEN the AuthorisationIntent is created and the launcher's launch function is called
+        verify(launcher).launch(any())
     }
 
-    @Test
-    fun unsuccessfulFinaliseCallbackResponse() {
-        val error = assertThrows(AuthenticationError::class.java) {
-            loginSession.finalise(Intent()) {}
+    @Test(expected = IllegalArgumentException::class)
+    fun finaliseThrowsIllegalArgumentExceptionForMalformedIntentResponse() {
+        // GIVEN an intent with a malformed (empty) response data json extra
+        val intent = Intent().apply {
+            putExtra(AuthorizationResponse.EXTRA_RESPONSE, "{}")
         }
-
-        assertEquals(AuthenticationError.ErrorType.OAUTH, error.type)
-        assertEquals("Auth response was null", error.message)
+        // WHEN finalise is called
+        appAuthSession.finalise(intent) {}
+        // THEN IllegalArgumentException is thrown
     }
 
     @Test
-    fun semiSuccessfulFinaliseCallbackResponse_nullTokenResponse() {
-        val intent = Intent().setData(Uri.parse("https://mobile.build.account.gov.uk/re" +
-            "direct?state=OpHOXMt1jUmnkEKLNqnH8Q&code=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsIm" +
-            "tpZCI6IjE2ZGI2NTg3LTU0NDUtNDVkNi1hN2Q5LTk4NzgxZWJkZjkzZCJ9.eyJpc3MiOiJodHRwczo" +
-            "vL3Rva2VuLmJ1aWxkLmFjY291bnQuZ292LnVrIiwic3ViIjoiYjA1YzQ2MmMtNmE3OS00NDgzLWJk" +
-            "YmYtMTEyMjIzYzZjNjI2IiwiYXVkIjoiaHR0cHM6Ly90b2tlbi5idWlsZC5hY2NvdW50Lmdvdi51ay" +
-            "IsImlhdCI6MTcyMzAyNjczMCwiZXhwIjoxNzIzMDI2OTEwfQ.NjKxaPm29CNtrUPIDPkjlTQX4bB" +
-            "1_JfMFduCTIjjcNY_VUCJmr0sjA08xYaq1cTY92F7umuk4oQjgtYV2gUfwg"))
-        intent.putExtra("net.openid.appauth.AuthorizationResponse", "{\"request\":" +
-            "{\"configuration\":{\"authorizationEndpoint\":\"https:\\/\\/token.build.account." +
-            "gov.uk\\/authorize\",\"tokenEndpoint\":\"https:\\/\\/token.build.account.gov.uk" +
-            "\\/token\"},\"clientId\":\"bYrcuRVvnylvEgYSSbBjwXzHrwJ\",\"responseType\":\"code\"" +
-            ",\"redirectUri\":\"https:\\/\\/mobile.build.account.gov.uk\\/redirect\",\"scope\"" +
-            ":\"openid\",\"ui_locales\":\"en\",\"state\":\"ZSP4n9sNkWAEEBwcIcMKHg\",\"nonce\":" +
-            "\"14481e71-df65-4e8a-8a15-e48afc21d99b\",\"codeVerifier\":\"Wg22z1mcHVSjjwxtjsRXl" +
-            "cGUnBp1Ilh46j30ZOKpl_9E_yPg0lyjzQ1dC0aaXAZlM-7LClB0jgPnc0MDrSl9Zg\",\"codeVerifier" +
-            "Challenge\":\"_kmKDDhAwultvWmu-7E4Gn7GO5xTFsQum2HCpY5EfaI\",\"codeVerifierChalleng" +
-            "eMethod\":\"S256\",\"additionalParameters\":{\"vtr\":\"[\\\"Cl.Cm.P0\\\"]\"}},\"st" +
-            "ate\":\"ZSP4n9sNkWAEEBwcIcMKHg\",\"code\":\"eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsIm" +
-            "tpZCI6IjE2ZGI2NTg3LTU0NDUtNDVkNi1hN2Q5LTk4NzgxZWJkZjkzZCJ9.eyJpc3MiOiJodHRwczovL3R" +
-            "va2VuLmJ1aWxkLmFjY291bnQuZ292LnVrIiwic3ViIjoiM2MyMTY0MjYtZTZkZC00YmY5LTg0ZWEtZjJlZ" +
-            "DRlM2EyY2ZkIiwiYXVkIjoiaHR0cHM6Ly90b2tlbi5idWlsZC5hY2NvdW50Lmdvdi51ayIsImlhdCI6MTc" +
-            "yMzAzMDYxNywiZXhwIjoxNzIzMDMwNzk3fQ.TuZnb9qOHSqd82G0190fHsLKcfr2QlV8ZvkmZNjbX6UK2v" +
-            "cz7KlIUolbjZtqZMjlWzZ3csrVTvYAUYyrTY2tGg\",\"additional_parameters\":{}}")
-        whenever(mockAuthService.performTokenRequest(any(), any())).thenAnswer {
+    fun finaliseThrowsAuthenticationErrorForIntentWithoutResponse() {
+        // GIVEN an (empty) intent without a response data json extra
+        val intent = Intent()
+        // WHEN finalise is called
+        val error = assertThrows(AuthenticationError::class.java) {
+            appAuthSession.finalise(intent) {}
+        }
+        // THEN an AuthenticationError is thrown
+        assertEquals(AuthenticationError.ErrorType.OAUTH, error.type)
+        assertEquals(AuthenticationError.NULL_AUTH_MESSAGE, error.message)
+    }
+
+    @Test
+    fun finaliseThrowsAuthenticationErrorOnCallbackNullResponse() {
+        // GIVEN a performTokenRequest that fails with an AuthorizationException
+        whenever(authService.performTokenRequest(any(), any())).thenAnswer {
             (it.arguments[1] as TokenResponseCallback).onTokenRequestCompleted(
                 null,
-                null
+                NETWORK_ERROR
             )
         }
 
-        val error = assertThrows(AuthenticationError::class.java) {
-            loginSession.finalise(intent) {}
+        val intent = Intent().apply {
+            putExtra(AuthorizationResponse.EXTRA_RESPONSE, TEST_EXTRA_VALUE)
         }
-
+        // WHEN finalise is called
+        val error = assertThrows(AuthenticationError::class.java) {
+            appAuthSession.finalise(intent) {}
+        }
+        // THEN an AuthenticationError is thrown
         assertEquals(AuthenticationError.ErrorType.OAUTH, error.type)
-        assertEquals("Failed token request", error.message)
+        assertEquals(NETWORK_ERROR_MSG, error.message)
+    }
+
+    companion object {
+        private const val NETWORK_ERROR_MSG = "Network error"
+        private val NETWORK_ERROR = AuthorizationException(0, 3, null, NETWORK_ERROR_MSG, null, null)
+        private const val TEST_EXTRA_VALUE = "{\"request\":" +
+                "{\"configuration\":{\"authorizationEndpoint\":\"https:\\/\\/token.build.account." +
+                "gov.uk\\/authorize\",\"tokenEndpoint\":\"https:\\/\\/token.build.account.gov.uk" +
+                "\\/token\"},\"clientId\":\"bYrcuRVvnylvEgYSSbBjwXzHrwJ\",\"responseType\":\"code\"" +
+                ",\"redirectUri\":\"https:\\/\\/mobile.build.account.gov.uk\\/redirect\",\"scope\"" +
+                ":\"openid\",\"ui_locales\":\"en\",\"state\":\"ZSP4n9sNkWAEEBwcIcMKHg\",\"nonce\":" +
+                "\"14481e71-df65-4e8a-8a15-e48afc21d99b\",\"codeVerifier\":\"Wg22z1mcHVSjjwxtjsRXl" +
+                "cGUnBp1Ilh46j30ZOKpl_9E_yPg0lyjzQ1dC0aaXAZlM-7LClB0jgPnc0MDrSl9Zg\",\"codeVerifier" +
+                "Challenge\":\"_kmKDDhAwultvWmu-7E4Gn7GO5xTFsQum2HCpY5EfaI\",\"codeVerifierChalleng" +
+                "eMethod\":\"S256\",\"additionalParameters\":{\"vtr\":\"[\\\"Cl.Cm.P0\\\"]\"}},\"st" +
+                "ate\":\"ZSP4n9sNkWAEEBwcIcMKHg\",\"code\":\"eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsIm" +
+                "tpZCI6IjE2ZGI2NTg3LTU0NDUtNDVkNi1hN2Q5LTk4NzgxZWJkZjkzZCJ9.eyJpc3MiOiJodHRwczovL3R" +
+                "va2VuLmJ1aWxkLmFjY291bnQuZ292LnVrIiwic3ViIjoiM2MyMTY0MjYtZTZkZC00YmY5LTg0ZWEtZjJlZ" +
+                "DRlM2EyY2ZkIiwiYXVkIjoiaHR0cHM6Ly90b2tlbi5idWlsZC5hY2NvdW50Lmdvdi51ayIsImlhdCI6MTc" +
+                "yMzAzMDYxNywiZXhwIjoxNzIzMDMwNzk3fQ.TuZnb9qOHSqd82G0190fHsLKcfr2QlV8ZvkmZNjbX6UK2v" +
+                "cz7KlIUolbjZtqZMjlWzZ3csrVTvYAUYyrTY2tGg\",\"additional_parameters\":{}}"
     }
 }
--- app/src/main/java/uk/gov/android/authentication/AppAuthSession.kt
+++ app/src/main/java/uk/gov/android/authentication/AppAuthSession.kt
@@ -3,12 +3,8 @@ package uk.gov.android.authentication
 import android.content.Context
 import android.content.Intent
 import androidx.activity.result.ActivityResultLauncher
-import net.openid.appauth.AuthorizationException
-import net.openid.appauth.AuthorizationRequest
 import net.openid.appauth.AuthorizationResponse
 import net.openid.appauth.AuthorizationService
-import net.openid.appauth.AuthorizationServiceConfiguration
-import java.util.UUID
 
 @Suppress("TooGenericExceptionThrown")
 class AppAuthSession(
@@ -20,69 +16,26 @@ class AppAuthSession(
         launcher: ActivityResultLauncher<Intent>,
         configuration: LoginSessionConfiguration
     ) {
-        with(configuration) {
-            val nonce = UUID.randomUUID().toString()
-
-            val serviceConfig =
-                AuthorizationServiceConfiguration(
-                    authorizeEndpoint,
-                    tokenEndpoint
-                )
-
-            val additionalParameters = mutableMapOf(
-                "vtr" to vectorsOfTrust
-            )
-            persistentSessionId?.let {
-                additionalParameters["govuk_signin_session_id"] = it
-            }
-
-            val builder =
-                AuthorizationRequest.Builder(
-                    serviceConfig,
-                    clientId,
-                    responseType.value,
-                    redirectUri
-                )
-                    .setScopes(scopes.map { it.value })
-                    .setUiLocales(locale.value)
-                    .setNonce(nonce)
-                    .setAdditionalParameters(additionalParameters)
-
-            val authRequest = builder.build()
-
-            val authIntent = authService.getAuthorizationRequestIntent(authRequest)
-            launcher.launch(authIntent)
-        }
+        val intent = authService.getAuthorizationRequestIntent(configuration.createRequest())
+        launcher.launch(intent)
     }
 
     override fun finalise(
         intent: Intent,
         callback: (tokens: TokenResponse) -> Unit
     ) {
-        val authorizationResponse = AuthorizationResponse.fromIntent(intent)
-
-        if (authorizationResponse == null) {
-            val exception = AuthorizationException.fromIntent(intent)
-
-            throw AuthenticationError(
-                exception?.message ?: "Auth response was null",
-                AuthenticationError.ErrorType.OAUTH
+        val authResponse = AuthorizationResponse.fromIntent(intent)
+        val request = authResponse?.createTokenExchangeRequest() ?: throw AuthenticationError.from(intent)
+        authService.performTokenRequest(
+            request
+        ) { response, exception ->
+            callback(
+                response?.toTokenResponse() ?: throw AuthenticationError.from(exception)
             )
         }
+    }
 
-        val exchangeRequest = authorizationResponse.createTokenExchangeRequest()
-
-        authService.performTokenRequest(
-            exchangeRequest
-        ) { response, exception ->
-            if (response == null) {
-                throw AuthenticationError(
-                    exception?.message ?: "Failed token request",
-                    AuthenticationError.ErrorType.OAUTH
-                )
-            }
-
-            callback(response.toTokenResponse())
-        }
+    companion object {
+        const val REQUEST_CODE_AUTH = 418 // Is this needed?
     }
 }
