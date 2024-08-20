ca004c78fb4ebf724c700b60eeaa092592e64e71
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-08-20 16:23:30 +0100

    test: Update AuthenticationError for increased code coverage
    Add static factory functions to simplify creation elsewhere and to group like tests
    Update AuthenticationErrorTest to cover construction and factory functions
    Swap to use kotlin test library
    
    Signed-off-by: ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>

--- app/src/main/java/uk/gov/android/authentication/AuthenticationError.kt
+++ app/src/main/java/uk/gov/android/authentication/AuthenticationError.kt
@@ -1,5 +1,8 @@
 package uk.gov.android.authentication
 
+import android.content.Intent
+import net.openid.appauth.AuthorizationException
+
 class AuthenticationError(
     override val message: String,
     val type: ErrorType
@@ -7,5 +10,16 @@ class AuthenticationError(
     enum class ErrorType {
         OAUTH,
         NETWORK
+    }
+
+    companion object {
+        internal const val NULL_AUTH_MESSAGE = "Auth response was null"
+
+        fun from(intent: Intent) = from(AuthorizationException.fromIntent(intent))
+
+        fun from(exception: AuthorizationException?) = AuthenticationError(
+            message = exception?.message ?: NULL_AUTH_MESSAGE,
+            type = ErrorType.OAUTH
+        )
     }
 }
