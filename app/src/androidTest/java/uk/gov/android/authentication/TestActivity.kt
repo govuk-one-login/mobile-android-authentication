b5025572eacf0e24166e82b929c4d7f9f848de62
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-08-22 10:41:32 +0100

    test: Add TestActivity to the androidTest package
    Add TestActivity with basic ActivityResultLauncher functionality
    Add AndroidManifest.xml to the androidTest package to register the TestActivity
    
    Signed-off-by: ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>

---
+++ app/src/androidTest/java/uk/gov/android/authentication/TestActivity.kt
@@ -0,0 +1,19 @@
+package uk.gov.android.authentication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class TestActivity : AppCompatActivity() {
+    var callback: (Intent) -> Unit = {}
+    lateinit var launcher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            callback(result.data ?: throw Exception(""))
+        }
+    }
+}
