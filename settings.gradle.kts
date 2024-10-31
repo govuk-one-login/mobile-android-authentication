494fb3769be4bdd6ae05686290bdd52858b83b05
ThomasIent <thomas.ient@digital.cabinet-office.gov.uk>
2024-10-31 09:50:51 +0000

    chore: Remove GitHub credentials work
    Update settings.gradle.kts to revert back to what it was
    
    Resolves: 10311

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.api.internal.provider.MissingValueException
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -8,44 +6,13 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         gradlePluginPortal()
         mavenCentral()
-        maven(
-            "https://maven.pkg.github.com/govuk-one-login/mobile-android-networking",
-            setupGithubCredentials()
-        )
-    }
-}
-
-fun setupGithubCredentials(): MavenArtifactRepository.() -> Unit =
-    {
-        val (credUser, credToken) = fetchGithubCredentials()
-        credentials {
-            username = credUser
-            password = credToken
-        }
-    }
-
-fun fetchGithubCredentials(): Pair<String, String> {
-    val gprUser = providers.gradleProperty("gpr.user")
-    val gprToken = providers.gradleProperty("gpr.token")
-
-    return try {
-        gprUser.get() to gprToken.get()
-    } catch (exception: MissingValueException) {
-        logger.warn(
-            "Could not find 'Github Package Registry' properties. Refer to the proceeding " +
-                    "location for instructions:\n\n" +
-                    "${rootDir.path}/docs/developerSetup/github-authentication.md\n",
-            exception
-        )
-
-        System.getenv("USERNAME") to System.getenv("TOKEN")
     }
 }
 
