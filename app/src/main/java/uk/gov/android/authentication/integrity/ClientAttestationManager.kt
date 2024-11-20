1ad7c652bf1b91bcff835c5ae5dd251836a156de
Alex Bradbury <alex.bradbury@digital.cabinet-office.gov.uk>
2024-11-20 15:33:38 +0000

    feat: - Add method to verify a given attestation matches the local public key - Add method to extract expiry time from attestation

--- app/src/main/java/uk/gov/android/authentication/integrity/ClientAttestationManager.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.android.authentication.integrity
-
-import uk.gov.android.authentication.integrity.appcheck.model.AttestationResponse
-import uk.gov.android.authentication.integrity.pop.SignedPoP
-
-interface ClientAttestationManager {
-    suspend fun getAttestation(): AttestationResponse
-    fun generatePoP(iss: String, aud: String): SignedPoP
-}
