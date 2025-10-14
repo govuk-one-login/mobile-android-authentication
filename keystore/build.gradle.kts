00c5bf2f14c7c1e026c10d092f11e15a70ea77d6
Bianca Mihaila <bianca.mihaila@digital.cabinet-office.gov.uk>
2025-10-14 16:43:56 +0100

    feat: Create key store module
    - move the KeyStoreManager package into it's own module, rather
    thank keeping it part of the app integrity (app) one since this can be
    used independently of the login or app integrity flow

---
+++ keystore/build.gradle.kts
@@ -0,0 +1,116 @@
+import uk.gov.pipelines.config.ApkConfig
+
+plugins {
+    `maven-publish`
+    alias(libs.plugins.kotlin.serlialization)
+    id("uk.gov.pipelines.android-lib-config")
+}
+
+apply(from = "${rootProject.extra["configDir"]}/detekt/config.gradle")
+apply(from = "${rootProject.extra["configDir"]}/ktlint/config.gradle")
+
+android {
+    defaultConfig {
+        val apkConfig: ApkConfig by project.rootProject.extra
+        namespace = apkConfig.applicationId + ".impl"
+        compileSdk = apkConfig.sdkVersions.compile
+        minSdk = apkConfig.sdkVersions.minimum
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    lint {
+        val configDir = "${rootProject.projectDir}/config"
+
+        abortOnError = true
+        absolutePaths = true
+        baseline = File("$configDir/android/baseline.xml")
+        checkAllWarnings = true
+        checkDependencies = false
+        checkGeneratedSources = false
+        checkReleaseBuilds = true
+        disable.addAll(
+            setOf(
+                "ConvertToWebp",
+                "UnusedIds",
+                "VectorPath"
+            )
+        )
+        explainIssues = true
+        htmlReport = true
+        ignoreTestSources = true
+        ignoreWarnings = false
+        lintConfig = File("$configDir/android/lint.xml")
+        noLines = false
+        quiet = false
+        showAll = true
+        textReport = true
+        warningsAsErrors = true
+        xmlReport = true
+    }
+
+    testOptions {
+        execution = "ANDROIDX_TEST_ORCHESTRATOR"
+        animationsDisabled = true
+        unitTests.all {
+            it.useJUnitPlatform()
+            it.testLogging {
+                events =
+                    setOf(
+                        org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED,
+                        org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED,
+                        org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+                    )
+            }
+        }
+        unitTests {
+            isReturnDefaultValues = true
+            isIncludeAndroidResources = true
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    listOf(
+        kotlin("test"),
+        kotlin("test-junit"),
+        libs.bundles.android.test,
+        libs.bundles.mockito,
+        libs.bundles.espresso,
+        project(":app")
+    ).forEach(::androidTestImplementation)
+
+    listOf(
+        libs.androidx.core.core.ktx,
+        libs.appcompat,
+        libs.material,
+        libs.bouncy.castle,
+        libs.kotlinx.serialization.json,
+        project(":app")
+    ).forEach(::implementation)
+
+    listOf(
+        kotlin("test"),
+        kotlin("test-junit5"),
+        libs.bundles.test,
+        platform(libs.junit.bom),
+        libs.mockito.kotlin
+    ).forEach(::testImplementation)
+}
---
+++ keystore/src/test/java/uk/gov/android/keystore/KeyStoreManagerTest.kt
@@ -0,0 +1,58 @@
+package uk.gov.android.keystore
+
+import java.math.BigInteger
+import java.security.spec.ECField
+import java.security.spec.ECParameterSpec
+import java.security.spec.ECPoint
+import java.security.spec.EllipticCurve
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class KeyStoreManagerTest {
+    @Test
+    fun `convert signature from DER format to ASN1 format`() {
+        val spec = ECParameterSpec(
+            EllipticCurve(
+                ECField { return@ECField 256 },
+                BigInteger(
+                    "11579208921035624876269744694940757353008614341529031419553" +
+                        "3631308867097853948"
+                ),
+                BigInteger(
+                    "41058363725152142129326129780047268409114441015993725554" +
+                        "835256314039467401291"
+                )
+            ),
+            ECPoint(
+                BigInteger(
+                    "4843956129390645175905258525279791420276294952604174799584" +
+                        "4080717082404635286"
+                ),
+                BigInteger(
+                    "3613425095674979579858512791958788195661110667298501507187" +
+                        "7198253568414405109"
+                )
+            ),
+            BigInteger(
+                "11579208921035624876269744694940757352999695522413576034242225" +
+                    "9061068512044369"
+            ),
+            1
+        )
+        val input = byteArrayOf(
+            48, 69, 2, 33, 0, -35, 79, -118, -2, -67, -74, 19, 59, 93, -39, -84,
+            57, 11, -38, 35, -55, 101, 14, -16, -57, 72, -2, -67, -28, 119, 97, 98, 118, -66, 62,
+            26, 120, 2, 32, 125, -110, -4, 61, -61, 53, -57, 20, -14, 96, 107, -13, -108, 12, -27,
+            78, 91, -103, 13, -17, 13, 85, -103, 42, 54, 122, 101, -28, -55, 26, 12, -48
+        )
+        val expected = byteArrayOf(
+            -35, 79, -118, -2, -67, -74, 19, 59, 93, -39, -84, 57, 11, -38,
+            35, -55, 101, 14, -16, -57, 72, -2, -67, -28, 119, 97, 98, 118, -66, 62, 26, 120, 125,
+            -110, -4, 61, -61, 53, -57, 20, -14, 96, 107, -13, -108, 12, -27, 78, 91, -103, 13,
+            -17, 13, 85, -103, 42, 54, 122, 101, -28, -55, 26, 12, -48
+        )
+
+        val result = KeyStoreManager.convertSignatureToASN1(input, spec)
+        assertEquals(expected.contentToString(), result.contentToString())
+    }
+}
