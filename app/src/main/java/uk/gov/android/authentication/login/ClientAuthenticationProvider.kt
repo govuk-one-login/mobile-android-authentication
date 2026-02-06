9031db6e3b3d71ddd1971743b555be900c4326bf
Chris Donnelly <205921253+chrisdondigital@users.noreply.github.com>
2026-02-06 16:22:29 +0000

    feat!: remove deprecated functions (#297)
    * feat: remove deprecated functions
    
    Resolves: DCMAW-16281
    
    * feat: make ClientAuthenticationProvider functional interface

--- app/src/main/java/uk/gov/android/authentication/login/ClientAuthenticationProvider.kt
+++ app/src/main/java/uk/gov/android/authentication/login/ClientAuthenticationProvider.kt
@@ -2,17 +2,7 @@ package uk.gov.android.authentication.login
 
 import net.openid.appauth.ClientAuthentication
 
-interface ClientAuthenticationProvider {
-    @Deprecated(
-        message = "Replace with setCustomClientAuthentication(...) that accepts a DPoP to " +
-            "allow for refresh tokens to be obtained - will be removed on 7th of December",
-        level = DeprecationLevel.WARNING
-    )
-    fun setCustomClientAuthentication(
-        clientAttestation: String?,
-        proofOfPossession: String?
-    ): ClientAuthentication
-
+fun interface ClientAuthenticationProvider {
     fun setCustomClientAuthentication(
         clientAttestation: String?,
         proofOfPossession: String?,
@@ -21,32 +11,6 @@ interface ClientAuthenticationProvider {
 }
 
 class ClientAuthenticationProviderImpl : ClientAuthenticationProvider {
-
-    @Deprecated(
-        "Replace with setCustomClientAuthentication(...) that accepts a DPoP to allow" +
-            " for refresh tokens to be obtained - will be removed on 7th of December",
-        ReplaceWith(
-            "uk/gov/android/authentication/login/ClientAuthenticationProvider" +
-                "#setCustomClientAuthentication"
-        ),
-        DeprecationLevel.WARNING
-    )
-    override fun setCustomClientAuthentication(
-        clientAttestation: String?,
-        proofOfPossession: String?
-    ): ClientAuthentication {
-        return object : ClientAuthentication {
-            override fun getRequestHeaders(clientId: String): MutableMap<String, String> =
-                mutableMapOf(
-                    Pair(CLIENT_ATTESTATION, clientAttestation ?: ""),
-                    Pair(PROOF_OF_POSSESSION, proofOfPossession ?: "")
-                )
-
-            override fun getRequestParameters(clientId: String): MutableMap<String, String> =
-                mutableMapOf()
-        }
-    }
-
     override fun setCustomClientAuthentication(
         clientAttestation: String?,
         proofOfPossession: String?,
